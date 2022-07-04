77c533b895592c409e09dbfe151256e62893bc4c
ivan-bortnik <ivan.bortnik@protonmail.com>
2022-07-04 11:23:46 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,85 @@
+function applyControlsToImage(img) {
+    
+    // Drag n drop
+    img.onmousedown = function (event) {
+        let remove = false;
+
+        let relX = img.getBoundingClientRect().x - event.pageX;
+        let relY = img.getBoundingClientRect().y - event.pageY;
+        
+        function move(event) {
+            // TODO: The image should not be centered on the mouse when moved
+            img.style.left = event.pageX + relX + 'px';
+            img.style.top = event.pageY + relY + 'px';
+
+            if (event.pageX > window.innerWidth - 50 && event.pageY > window.innerHeight - 50) {
+                remove = true;
+                document.querySelector('#bin').style.width = '900px';
+                document.querySelector('#bin').style.height = '900px';
+                document.querySelector('#bin').style.z_index = 300;
+            } else {
+                remove = false;
+                document.querySelector('#bin').style.width = '70px';
+                document.querySelector('#bin').style.height = '70px';
+                document.querySelector('#bin').style.z_index = 1;
+            }
+        }
+    
+        document.addEventListener('mousemove', move);
+    
+        document.onmouseup = function (event) {
+            document.removeEventListener('mousemove', move);
+
+            if (remove)  {
+                img.remove();
+                document.querySelector('#bin').style.width = '70px';
+                document.querySelector('#bin').style.height = '70px';
+                document.querySelector('#bin').style.z_index = 1;
+            }
+        };
+    
+    };
+
+    // Resize
+    img.addEventListener('mouseenter', () => {
+        function resize(event) {
+            img.style.height = img.offsetHeight - event.deltaY / 2 + 'px';
+        }
+
+        window.addEventListener('wheel', resize);
+
+        img.addEventListener('mouseleave', () => {
+            window.removeEventListener('wheel', resize);
+        });
+    });
+    
+}
+
+
+// NOT MY CODE
+// https://htmldom.dev/paste-an-image-from-the-clipboard/
+document.addEventListener('paste', function (evt) {
+    // Get the data of clipboard
+    const clipboardItems = evt.clipboardData.items;
+    const items = [].slice.call(clipboardItems).filter(function (item) {
+        // Filter the image items only
+        return item.type.indexOf('image') !== -1;
+    });
+    if (items.length === 0) {
+        return;
+    }
+
+    const item = items[0];
+    // Get the blob of image
+    const blob = item.getAsFile();
+
+    let newImg = document.createElement('img');
+    
+    newImg.style.height = '300px';
+    newImg.classList.add('item');
+    newImg.draggable = false;
+    applyControlsToImage(newImg);
+    newImg.src = URL.createObjectURL(blob);
+    
+    document.body.appendChild(newImg);
+});
